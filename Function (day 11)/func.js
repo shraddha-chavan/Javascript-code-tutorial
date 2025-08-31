aeaea0b6c148a88a3bd87461aaf8e04754520515
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-08-31 20:08:47 +0530

    Add files via upload

---
+++ Function (day 11)/func.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    console.log("Shraddha Chavan");
+    console.log("Sunil Chavan");
+}
+myFunction();
+
+
+/////////////////////
+
+function myname(){
+    //parameter->input
+    console.log(msg);
+}
+myname("I am shraddha");//argument
